1b5929e652e7c90655fb963cea6d9b97cd7d62cd
Shefali Kaushal <shefali2404kaushal@gmail.com>
2024-08-06 11:33:13 +0530

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
   const[category,setCategory]=useState("general");
   return (
     <div>
-      
       <Navbar setCategory={setCategory}/>
       <NewsBoard category={category}/>
     </div>
